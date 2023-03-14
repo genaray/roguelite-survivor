4c9f8ffd40ffe429e1510956d68d1ebad06c9c65
Micah Caunter <65076703+proc-gen@users.noreply.github.com>
2023-03-14 16:53:23 -0400

    feat: add error logging to text file (#43)

--- RogueliteSurvivor/RogueliteSurvivor/Program.cs
+++ RogueliteSurvivor/RogueliteSurvivor/Program.cs
@@ -1,5 +1,19 @@
-﻿
-using (var game = new RogueliteSurvivor.Game1())
+﻿using System;
+using System.IO;
+
+try
 {
-    game.Run();
+    using (var game = new RogueliteSurvivor.Game1())
+    {
+        game.Run();
+    }
+}
+catch(Exception ex)
+{
+    if (!Directory.Exists(Path.Combine("Logs")))
+    {
+        Directory.CreateDirectory(Path.Combine("Logs"));
+    }
+
+    File.WriteAllText(Path.Combine("Logs", string.Concat("error-", DateTime.Now.ToString("yyyyMMddHHmmss"), ".txt")), ex.ToString());
 }
